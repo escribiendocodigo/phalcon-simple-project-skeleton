5a3ab1b56306131714fef83d4c68a8e757bf8057
M4nu <m4nu.coder@gmail.com>
2023-05-10 00:50:44 -0500

    Prettier code

--- app/config/services.php
+++ app/config/services.php
@@ -14,109 +14,138 @@ use Phalcon\Mvc\Url as UrlResolver;
 /**
  * Shared configuration service
  */
-$di->setShared('config', function () {
-    return include APP_PATH . "/config/config.php";
-});
+$di->setShared(
+    'config',
+    function () {
+        return include APP_PATH . "/config/config.php";
+    }
+);
 
 /**
  * The URL component is used to generate all kind of urls in the application
  */
-$di->setShared('url', function () {
-    $config = $this->getConfig();
+$di->setShared(
+    'url',
+    function () {
+        $config = $this->getConfig();
 
-    $url = new UrlResolver();
-    $url->setBaseUri($config->application->baseUri);
+        $url = new UrlResolver();
+        $url->setBaseUri($config->application->baseUri);
 
-    return $url;
-});
+        return $url;
+    }
+);
 
 /**
  * Setting up the view component
  */
-$di->setShared('view', function () {
-    $config = $this->getConfig();
+$di->setShared(
+    'view',
+    function () {
+        $config = $this->getConfig();
 
-    $view = new View();
-    $view->setDI($this);
-    $view->setViewsDir($config->application->viewsDir);
+        $view = new View();
+        $view->setDI($this);
+        $view->setViewsDir($config->application->viewsDir);
 
-    $view->registerEngines([
-        '.volt' => function ($view) {
-            $config = $this->getConfig();
+        $view->registerEngines(
+            [
+                '.volt' => function ($view) {
+                    $config = $this->getConfig();
 
-            $volt = new VoltEngine($view, $this);
+                    $volt = new VoltEngine($view, $this);
 
-            $volt->setOptions([
-                'path' => $config->application->cacheDir,
-                'separator' => '_'
-            ]);
+                    $volt->setOptions(
+                        [
+                            'path' => $config->application->cacheDir,
+                            'separator' => '_'
+                        ]
+                    );
 
-            return $volt;
-        },
-        '.phtml' => PhpEngine::class
+                    return $volt;
+                },
+                '.phtml' => PhpEngine::class
+            ]
+        );
 
-    ]);
-
-    return $view;
-});
-
-/**
- * Database connection is created based in the parameters defined in the configuration file
- */
-$di->setShared('db', function () {
-    $config = $this->getConfig();
-
-    $class = 'Phalcon\Db\Adapter\Pdo\\' . $config->database->adapter;
-    $params = [
-        'host'     => $config->database->host,
-        'username' => $config->database->username,
-        'password' => $config->database->password,
-        'dbname'   => $config->database->dbname,
-        'charset'  => $config->database->charset
-    ];
-
-    if ($config->database->adapter == 'Postgresql') {
-        unset($params['charset']);
+        return $view;
     }
-
-    return new $class($params);
-});
-
+);
 
 /**
- * If the configuration specify the use of metadata adapter use it or use memory otherwise
+ * Database connection is created based in the parameters defined
+ * in the configuration file
  */
-$di->setShared('modelsMetadata', function () {
-    return new MetaDataAdapter();
-});
+$di->setShared(
+    'db',
+    function () {
+        $config = $this->getConfig();
+
+        $class = 'Phalcon\Db\Adapter\Pdo\\' . $config->database->adapter;
+        $params = [
+            'host'     => $config->database->host,
+            'username' => $config->database->username,
+            'password' => $config->database->password,
+            'dbname'   => $config->database->dbname,
+            'charset'  => $config->database->charset
+        ];
+
+        if ($config->database->adapter == 'Postgresql') {
+            unset($params['charset']);
+        }
+
+        return new $class($params);
+    }
+);
+
+/**
+ * If the configuration specify the use of metadata adapter
+ * use it or use memory otherwise
+ */
+$di->setShared(
+    'modelsMetadata',
+    function () {
+        return new MetaDataAdapter();
+    }
+);
 
 /**
  * Register the session flash service with the Twitter Bootstrap classes
  */
-$di->set('flash', function () {
-    $escaper = new Escaper();
-    $flash = new Flash($escaper);
-    $flash->setImplicitFlush(false);
-    $flash->setCssClasses([
-        'error'   => 'alert alert-danger',
-        'success' => 'alert alert-success',
-        'notice'  => 'alert alert-info',
-        'warning' => 'alert alert-warning'
-    ]);
+$di->set(
+    'flash',
+    function () {
+        $escaper = new Escaper();
+        $flash = new Flash($escaper);
+        $flash->setImplicitFlush(false);
+        $flash->setCssClasses(
+            [
+                'error'   => 'alert alert-danger',
+                'success' => 'alert alert-success',
+                'notice'  => 'alert alert-info',
+                'warning' => 'alert alert-warning'
+            ]
+        );
 
-    return $flash;
-});
+        return $flash;
+    }
+);
 
 /**
  * Start the session the first time some component request the session service
  */
-$di->setShared('session', function () {
-    $session = new SessionManager();
-    $files = new SessionAdapter([
-        'savePath' => sys_get_temp_dir(),
-    ]);
-    $session->setAdapter($files);
-    $session->start();
+$di->setShared(
+    'session',
+    function () {
+        $session = new SessionManager();
+        $files = new SessionAdapter(
+            [
+                'savePath' => sys_get_temp_dir(),
+            ]
+        );
+        $session->setAdapter($files);
+        $session->start();
 
-    return $session;
-});
+        return $session;
+    }
+);
